8f3f071ad964d98b15eef90d1312f74cc91b9f6e
Rindra-Aniko <rindra.aniko@gmail.com>
2026-05-20 22:30:09 +0700

    Perf: pembersihan total CDN eksternal dan optimasi font lokal

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ export default function(eleventyConfig) {
     // konfigurasi Anda
   eleventyConfig.addPassthroughCopy("./src/images/");
     eleventyConfig.addPassthroughCopy("./src/js/");
-    eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/includes/css/");
+  eleventyConfig.addPassthroughCopy({ "src/_includes/fonts": "fonts" });
 
   return {
     dir: {
